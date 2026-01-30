a881ff0bd6535235fe51d9037cc4fbb662ed7d34
danilop418 <daniel.lopgon.4@educa.jcyl.es>
2026-01-30 08:49:46 +0100

    Reduzco el tamaño del logo para pruebas a 25 25 25

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function staticLoadPlaces() {
         latitude: 40.65574412076483,
         longitude: -4.699638143293775,
         model: "./assets/daremapp/daremapp_logo.glb",
-        scale: "50 50 50",
+        scale: "25 25 25",
         rotation: "0 0 0"
     };
 
