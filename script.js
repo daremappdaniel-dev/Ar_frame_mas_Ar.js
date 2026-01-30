109d370b071e97fce8f36a3edc3166c8aa48aa63
danilop418 <daniel.lopgon.4@educa.jcyl.es>
2026-01-30 08:56:23 +0100

    Tamaño minimo

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function staticLoadPlaces() {
         latitude: 40.65574412076483,
         longitude: -4.699638143293775,
         model: "./assets/daremapp/daremapp_logo.glb",
-        scale: "5 5 5",
+        scale: "0.5 0.5 0.5",
         rotation: "0 0 0"
     };
 
